3005dbc700b022440ce459732678bfdafd1b9f94
21117 <127743121+21117@users.noreply.github.com>
2023-04-17 20:32:04 +0900

    Add files via upload

--- lcd_cds_ar_com.ino
+++ lcd_cds_ar_com.ino
@@ -2,62 +2,44 @@
 #include <LiquidCrystal_I2C.h>
 #include <SimpleDHT.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-int cds = A0; // Analog input pin that the potentiometer is attached to
-int cdsValue = 0;
+LiquidCrystal_I2C lcd(0x27,16,2);
 
-int pinDHT11 = 2;
-SimpleDHT11 dht11(pinDHT11);
+const int cds = A0;
+
+SimpleDHT11 dht11(2);
 
 void setup()
 {
   Serial.begin(9600);
-  
-  lcd.init();                      // initialize the lcd 
   lcd.init();
-  // Print a message to the LCD.
   lcd.backlight();
-  lcd.setCursor(3,0);
-  lcd.print("Hello, world!");
-  lcd.setCursor(2,1);
-  lcd.print("Ywrobot Arduino!");
-   lcd.setCursor(0,2);
-  lcd.print("Arduino LCM IIC 2004");
-   lcd.setCursor(2,3);
-  lcd.print("Power By Ec-yuan!");
 }
-
 
 void loop()
 {
-  cdsValue  = analogRead(cds); // read the analog in value:
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("cds = ");delay(100);
+  int cdsValue  = analogRead(cds);
 
-Serial.println("=================================");
-  Serial.println("Sample DHT11...");
-byte temperature = 0;
+  byte temperature = 0;
   byte humidity = 0;
-  int err = SimpleDHTErrSuccess;
-
   
-if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
     Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
     return;
   }
+ 
+  lcd.setCursor(0,0);
+  lcd.print("cds = ");
+  lcd.print((int)cdsValue); 
 
-  lcd.backlight();
   lcd.setCursor(1,1);
-  Serial.print((int)temperature); 
-  lcd.print(" *C, "); 
+  lcd.print("T = "); 
+  lcd.print((int)temperature);
 
-  lcd.backlight();
-  lcd.setCursor(1,8);
-  Serial.print((int)humidity); 
-  lcd.println(" H");
+  lcd.setCursor(8,1);
+  lcd.println("H = ");
+  lcd.print((int)humidity);
   
-  // DHT11 sampling rate is 1HZ.
-  delay(1500);
+  delay(1000);
 }
